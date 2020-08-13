fd4fd03ffa89e6be67a2f896ae8b573c53ce1e2e
Alessio Cialini <alessio.cialini.sia@gmail.com>
2020-08-13 18:13:07 +0200

    [bpd-ms-point-processor/develop] updated WinningTransaction model and deployment.yml

--- integration/rest/src/main/java/it/gov/pagopa/bpd/point_processor/connector/winning_transaction/model/WinningTransaction.java
+++ integration/rest/src/main/java/it/gov/pagopa/bpd/point_processor/connector/winning_transaction/model/WinningTransaction.java
@@ -4,6 +4,10 @@ import it.gov.pagopa.bpd.point_processor.connector.winning_transaction.model.enu
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
@@ -20,27 +24,45 @@ import java.time.OffsetDateTime;
 @EqualsAndHashCode(of = {"idTrxAcquirer", "acquirerCode", "trxDate"}, callSuper = false)
 public class WinningTransaction {
 
+    @NotNull
+    @NotBlank
     String idTrxAcquirer;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
     String acquirerCode;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     OffsetDateTime trxDate;
 
+    @NotNull
+    @NotBlank
     String hpan;
 
+    @NotNull
     OperationType operationType;
 
+    @NotNull
+    @NotBlank
+    @Size(min = 2, max = 2)
+    @Pattern(regexp = "[0-9]{2}")
     String circuitType;
 
+    @NotNull
+    @NotBlank
     String idTrxIssuer;
 
     String correlationId;
 
+    @NotNull
     BigDecimal amount;
 
+    @Size(max = 3)
     String amountCurrency;
 
+    @NotNull
+    @NotBlank
     String mcc;
 
     String mccDescription;
@@ -49,8 +71,21 @@ public class WinningTransaction {
 
     Long awardPeriodId;
 
+    @NotNull
+    @NotBlank
     String acquirerId;
 
+    @NotNull
+    @NotBlank
     String merchantId;
+
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "([0-9]{6}|[0-9]{8})")
+    String bin;
+
+    @NotNull
+    @NotBlank
+    String terminalId;
 
 }
